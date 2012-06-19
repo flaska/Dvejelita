00da6f9b5642fe910c59219f62319478a361e07c
flaska <jakub.flaska@gmail.com>
2012-06-19 19:30:28 +0200

    'x'

--- login.php
+++ login.php
@@ -9,32 +9,45 @@
 			body {
 				height: 100%;
 				margin: 0px 0px 0px 0px;
+				color: #42210B;
+				font-style: italic;
 			}
 			div#gap-before-main-content{
 				height: 150px;
 				background: url('images/headerbg.png') repeat-x;
+			}
+			img#logo {
+				margin-left: 20%;
+				margin-right: auto;
 			}
 			div#main-content {
 				width: 30%;
 				min-width: 500px;
 				margin-left: auto;
 				margin-right: auto;
+				margin-top: 50px;
 			}
 			h1 {
 				font-size:1.5em;
 				text-align: center;
+				color: #42210B;
+				font-style: italic;
 			}
 			h2 {
 				font-size:1em;
 				text-align: center;
+				color: #42210B;
+				font-style: italic;
 			}
 			form {
 				width: 60%;
 				margin-left: auto;
 				margin-right: auto;
-				border-style: solid;
+				border-style: ridge;
 				border-width: 4px;
+				border-color: rgb(255,214,77);
 				padding: 10px 20px 10px 20px;
+				box-shadow: 3px 3px 5px 6px #ccc;
 			}
 			form .form-element {
 				width: 50%;
@@ -51,6 +64,8 @@
 			form input {
 				border: 0px;
 				outline: 1px solid #006;
+				background-color: #f9e25e;
+				text-align: center;
 			}
 			form label{
 				float: left;
@@ -79,13 +94,14 @@
 	</head>
 	<body>
 		<div id="gap-before-main-content">
+			<img id="logo" src="images/logo.png"/>
 		</div>
 		<div id="main-content">
 			<h1>Administrátorské rozhraní stránek Dvejelita.cz</h1>
 			<h2>Pro vstup je potřeba se přihlásit.</h2>
 			<form action="admin.php" method="post">
 				<label class="form-element">Zadejte heslo:</label>
-				<input type="text" class="form-element" name="password" />
+				<input type="password" class="form-element" name="password" />
 				<input id="submit" class="form-element" type="submit" value="Pokračovat" />
 			</form>
 		</div>
